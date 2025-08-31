ea921c45bd4075d1e839e8297e9ca4047f67589c
Jktu2023 <161673133+Jktu2023@users.noreply.github.com>
2025-08-31 19:36:54 +0700

    111

--- main.py
+++ main.py
@@ -1,32 +1,34 @@
 import asyncio # Запускает асинхронную функцию main()
-from aiogram import Bot, Dispatcher, F # Основные классы aiogram: бот и диспетчер (обработчик событий) и Специальный объект для фильтрации сообщений (например, по тексту или типу)
+from aiogram import Bot, Dispatcher, F # Основные классы aiogram: бот и диспетчер (обработчик событий) и Специальный объект
+# для фильтрации сообщений (например, по тексту или типу)
 from aiogram.types import Message # Тип для аннотации — чтобы IDE подсказывала методы
 from aiogram.filters import CommandStart, Command # Фильтры для команд /start, /help и т.д.
-# from aiogram.contrib.fsm_storage.memory import MemoryStorage
-# from handlers import dp
 from config import TOKEN_BOT, OPENWEATHER_API_KEY
 import random
-import httpx
+import httpx # httpx - библиотека для асинхронного HTTP-клиента
 
 bot = Bot(token=TOKEN_BOT) # экземпляр бота, который будет общаться с Telegram API.
 dp = Dispatcher() # диспетчер, отвечает за прослушивание входящих обновлений от Telegram (сообщений, команд, фото и т.д.).
-                        # Он решает, какую функцию вызвать в ответ на то или иное сообщение.
+                        # Он решает, какую функцию выбрать и вызвать в ответ на то или иное сообщение.
 
 @dp.message(Command('photo')) # это декоратор в библиотеке aiogram (3.x), который указывает диспетчеру (Dispatcher):
                               # «Эту функцию нужно вызывать, когда приходит сообщение, подходящее под указаные условия.»
 async def photo(message: Message): # Обработчик команды /photo
     list = ['https://epoi.ru/wa-data/public/shop/products/32/01/132/images/870/870.970.jpg','https://epoi.ru/wa-data/public/shop/products/32/01/132/images/24/24.970.jpg', 'https://epoi.ru/wa-data/public/shop/products/32/01/132/images/434/434.970.jpg', 'https://epoi.ru/wa-data/public/shop/products/32/01/132/images/434/434.970.jpg', 'https://epoi.ru/wa-data/public/shop/products/32/01/132/images/424/424.970.jpg', 'https://epoi.ru/wa-data/public/shop/products/32/01/132/images/716/716.970.jpg', 'https://epoi.ru/wa-data/public/shop/products/32/01/132/images/900/900.740x484.jpg', 'https://epoi.ru/wa-data/public/shop/products/32/01/132/images/715/715.970.jpg']
     rand_photo = random.choice(list)
-    await message.answer_photo(photo=rand_photo, caption='Это супер крутая картинка')
+    await message.answer_photo(photo=rand_photo, caption='Это супер крутая картинка') #  «await - Подожди, пока это действие (например, отправка фото в Telegram)
+    # завершится, прежде чем переходить к следующей строке». async и await — основа асинхронного кода. Где можно использовать await? Только внутри async def функций!
 
 @dp.message(F.photo) # Фильтр для фотографий F.photo — значит "если сообщение содержит фото".
 async def react_photo(message: Message):
     list = ['Ого, какая фотка!', 'Класс!', 'Не отправляй мне такое больше', 'Много интересного у тебя!', 'Прикольная фотка!', 'Супер!', 'Круто!']
     rand_answ = random.choice(list)
     await message.answer(rand_answ)
+
 @dp.message(F.text == "что такое ИИ?") # Фильтр для текста F.text — значит "если сообщение содержит текст".  F.text == "..." — строгое сравнение текста.
 async def aitext(message: Message): # Обработчик текста
-    await message.answer('Искусственный интеллект — это свойство искусственных интеллектуальных систем')
+    await message.answer('Искусственный интеллект — это свойство искусственных интеллектуальных систем...')
+
 @dp.message(CommandStart()) # Обработчик команды /start
 async def start(message: Message):
     await message.answer("Приветики, я бот!")
@@ -34,7 +36,6 @@ async def start(message: Message):
 @dp.message(Command('help')) #
 async def help(message: Message): # Обработчик команды /help
     await message.answer("Этот бот умеет принимать твои фотографии с коментариями и еще выполнять команды:\n/start\n/help\n/photo\n/weather")
-
 
 @dp.message(Command('weather'))
 async def weather_tomsk(message: Message): # Объявление асинхронной функции (обязательно для aiogram).
@@ -49,7 +50,7 @@ async def weather_tomsk(message: Message): # Объявление асинхро
             print(response.status_code)
             print(data)
 
-            if response.status_code == 200:
+            if response.status_code == 200: # Если запрос прошел успешно
                 temp = data['main']['temp']
                 sea_level = data['main']['sea_level']
                 feels_like = data['main']['feels_like']
@@ -83,7 +84,7 @@ if __name__ == "__main__":
     asyncio.run(main())
 
 
-# Как это работает?
+# Как это работает?  !
 # Когда пользователь что-то пишет боту:
 # Telegram отправляет обновление (update).
 # Dispatcher (dp) проверяет все зарегистрированные обработчики.
